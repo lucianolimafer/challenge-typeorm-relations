0f57a9c7785f87e11262a5c258e98807653f2b99
lucianolimafer <lucianolimaf3r@gmail.com>
2021-01-15 16:42:29 -0300

    :tada: initial commit

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customersExists = await this.customersRepository.findByEmail(email);
+
+    if (customersExists) {
+      throw new AppError('This email is already assigned on another customer');
+    }
+
+    const customer = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return customer;
   }
 }
 
